e7a2fa2738d2425ae99626211efb9407057c13d8
Ahmed <ahmedabidi3234@gmail.com>
2026-03-25 11:43:18 +0100

    chore: Remove turbopack root configuration from `next.config.ts`.

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  turbopack: {
-    root: "c:/Users/me/Desktop/Projects/calendly-style-booking-system/",
-  },
 };
 
 export default nextConfig;
